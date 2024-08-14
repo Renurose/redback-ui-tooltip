6a8ffcd3fc57771c9182ebc2fa14c52703668436
Renurose <renurose24022gmail.com>
2024-08-14 11:54:21 +1000

    Added tooltip components

---
+++ src/components/Tooltip/Tooltip.test.tsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react';
+import { Tooltip } from './Tooltip';
+ 
+describe('<Tooltip />', () =>{
+it('renders the tooltip component', () => {
+const { getByText } = render( <Tooltip text="Hover over me" tooltip="Tooltip text" /> ); expect(getByText('Hover over me')).toBeInTheDocument(); });
+});
+ 
